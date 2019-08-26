64b3915a08cab52d61c010e5d70a57cd8a0aea8b
hemant.nagpure <hemant.nagpure@concirrus.com>
2019-08-26 19:56:24 +0530

    Added logs

--- src/main/java/com/spring/batch/DBToCSV/config/PreferenceReaderConfig.java
+++ src/main/java/com/spring/batch/DBToCSV/config/PreferenceReaderConfig.java
@@ -2,6 +2,8 @@ package com.spring.batch.DBToCSV.config;
 
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -12,17 +14,20 @@ import com.spring.batch.DBToCSV.model.PreferenceRowMapper;
 
 @Configuration
 public class PreferenceReaderConfig {
-	
+
+	private static final Logger LOG = LoggerFactory.getLogger(PreferenceReaderConfig.class);
+
 	@Autowired
 	private DataSource ds;
 
 	@Bean
-	 public JdbcCursorItemReader<Preference> reader(){
-	  JdbcCursorItemReader<Preference> reader = new JdbcCursorItemReader<Preference>();
-	  reader.setDataSource(ds);
-	  reader.setSql("select * from preference");
-	  reader.setRowMapper(new PreferenceRowMapper());
-	  
-	  return reader;
-	 }
+	public JdbcCursorItemReader<Preference> reader() {
+		JdbcCursorItemReader<Preference> reader = new JdbcCursorItemReader<Preference>();
+		reader.setDataSource(ds);
+		reader.setSql("select * from preference");
+		reader.setRowMapper(new PreferenceRowMapper());
+
+		LOG.info("********* PREFERENCE READER CONFIGURED SUCCESSFULLY - {} *************", reader);
+		return reader;
+	}
 }
