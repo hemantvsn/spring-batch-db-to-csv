7a4aa6cfae99bd692cfe17559ead4e3701b2b8fd
hemant.nagpure <hemant.nagpure@concirrus.com>
2019-08-26 19:11:11 +0530

    First commit

---
+++ src/main/java/com/spring/batch/DBToCSV/DbToCsvApp.java
@@ -0,0 +1,13 @@
+package com.spring.batch.DBToCSV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbToCsvApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbToCsvApp.class, args);
+	}
+
+}
