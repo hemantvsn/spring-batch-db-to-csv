c12d0d3795a05c1b262bf98a22a5905b96585456
hemant.nagpure <hemant.nagpure@concirrus.com>
2019-08-26 19:22:18 +0530

    Added DB Config

---
+++ src/main/java/com/spring/batch/DBToCSV/config/DBConfig.java
@@ -0,0 +1,28 @@
+package com.spring.batch.DBToCSV.config;
+
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DBConfig {
+
+	private static final Logger LOG = LoggerFactory.getLogger(DBConfig.class);
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setDriverClassName("com.mysql.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://localhost:3306/test");
+		ds.setUsername("root");
+		ds.setPassword("root");
+
+		LOG.info("******* DATABASE INITIALIZED SUCCESSFULLY *********");
+		return ds;
+
+	}
+}
