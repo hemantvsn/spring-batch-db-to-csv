9b0ab455ea14a652e07e9f2336e6712c7a57c7fe
hemant.nagpure <hemant.nagpure@concirrus.com>
2019-08-26 19:22:25 +0530

    Main Class

--- src/main/java/com/spring/batch/DBToCSV/DbToCsvApp.java
+++ src/main/java/com/spring/batch/DBToCSV/DbToCsvApp.java
@@ -1,13 +1,19 @@
 package com.spring.batch.DBToCSV;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DbToCsvApp {
 
+	private static final Logger LOG = LoggerFactory.getLogger(DbToCsvApp.class);
+	
 	public static void main(String[] args) {
+		LOG.info("STARTING APPLICATION");
 		SpringApplication.run(DbToCsvApp.class, args);
+		LOG.info("APPLICATION HAS STARTED");
 	}
 
 }
